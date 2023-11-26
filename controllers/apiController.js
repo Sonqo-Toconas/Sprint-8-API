4ae20131a7af5cabcd0a6ca74c5c3eb6be14acc4
Sonqo-Toconas <Sonqo-Toconas>
2023-11-25 22:10:56 -0300

    Ruta users y categories agregada

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,7 +1,33 @@
 const db = require('../database/models');
 
 const api = {
+    users: async (req, res) => {
+        let users = await db.User.findAll()
 
+        res.status(200).json({
+            count: users.length,
+            users: users.map(user => ({
+                id: user.id_user,
+                name: user.name,
+                email: user.email,
+                detail: "/api/users/" + user.id_user
+            })),
+            status: 200
+        });
+    },
+
+    categories: async (req, res) => {
+        let categories = await db.Category.findAll()
+
+        res.status(200).json({
+            count: categories.length,
+            categories: categories.map(category => ({
+                id: category.id_category,
+                name: category.name
+            })),
+            status: 200
+        });
+    }
 }
 
 module.exports = api
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const router = express. Router();
+const router = express.Router();
 const apiController = require('../controllers/apiController');
+
+router.get('/users', apiController.users);
+router.get('/categories', apiController.categories);
 
 module.exports = router;
